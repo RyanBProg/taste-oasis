e063259503e728ecf79aa3f14f4700d0e17ff100
Ryan <ryanbprog@gmail.com>
2024-05-22 14:33:43 +0800

    small style changes: -gradient at the bottom change to orange -get in touch button color changed to suit

--- src/app/components/home/ActionBanner.tsx
+++ src/app/components/home/ActionBanner.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function ActionBanner() {
   return (
-    <section className="flex justify-center items-center bg-gradient-to-b from-white via-orange-400 to-red-500 py-8 px-4">
+    <section className="flex justify-center items-center bg-gradient-to-b from-white via-orange-400 to-orange-600 py-8 px-4">
       <div className="bg-white bg-opacity-15 rounded-md flex flex-col md:flex-row items-center">
         <Image
           src="/poke-banner.jpeg"
@@ -21,7 +21,7 @@ export default function ActionBanner() {
             Have questions or need assistance? Our team is here to help. Visit
             our contact page for more information on how to reach us.
           </p>
-          <button className="bg-orange-400 text-gray-50 font-semibold p-4 rounded-md mt-6 md:mt-2 hover:bg-orange-500">
+          <button className="bg-orange-200 text-black font-semibold p-4 rounded-md mt-6 md:mt-2 hover:bg-orange-300">
             <Link href="/contact">Get in Touch</Link>
           </button>
         </div>
