004410f959c5a80f8f60de6333a5ec7f2afd8eca
Ryan <ryanbprog@gmail.com>
2024-05-22 14:34:52 +0800

    about page complete: -copy added with images -tailwind styles complete

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "About Us | Taste Oasis",
@@ -8,8 +9,36 @@ export const metadata: Metadata = {
 
 export default function About() {
   return (
-    <>
-      <div>About Us</div>
-    </>
+    <div className="px-4 sm:text-center mt-14 mb-20">
+      <h1 className="text-6xl sm:text-7xl font-bold text-black">
+        <span className="text-orange-400">Our</span> Story
+      </h1>
+      <p className="text-gray-700 py-6 sm:max-w-[550px] sm:mx-auto">
+        Taste Oasis was born out of a passion for bringing the vibrant and
+        diverse flavors of Asia to our community. Founded by Steven, a culinary
+        enthusiast with a deep love for Asian cuisine, Taste Oasis is more than
+        just a restaurant – it's a journey through the rich culinary traditions
+        of Asia.
+      </p>
+      <div className="flex flex-col justify-center items-center gap-4 sm:flex-row">
+        <Image
+          src="/chef-steven.jpeg"
+          height={960}
+          width={640}
+          alt="portait of chef steven cooking"
+          className="rounded-md h-[480px] w-[320px]"
+        />
+        <div className="flex flex-col gap-2 bg-orange-300 bg-opacity-70 p-5 rounded-md max-w-[500px]">
+          <h2 className="text-2xl font-semibold">Meet Steven</h2>
+          <p className="text-gray-800 text-left">
+            Taste Oasis was born out of a passion for bringing the vibrant and
+            diverse flavors of Asia to our community. Founded by Steven, a
+            culinary enthusiast with a deep love for Asian cuisine, Taste Oasis
+            is more than just a restaurant – it's a journey through the rich
+            culinary traditions of Asia.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
